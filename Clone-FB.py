cec2bc374cc6b0eddb764e6d605e548acb3c53a4
Swapon-Vau <55379141+Swapon-Vau@users.noreply.github.com>
2023-01-09 21:17:01 +0600

    Clone-FB.py

--- Clone-FB.py
+++ Clone-FB.py
@@ -397,14 +397,14 @@ logo = ("""
 \x1b[1;92m      
 \x1b[1;92m      
 
-   \x1b[1;92m╔═══════════════════════════════════╗
+   \x1b[1;92m╔═════════════════════════════════════╗
    \x1b[1;92m║➣TOOL NAME : {Cyber 71}              ║
    \x1b[1;92m║➣AUTHOR    : SWAPON MIA              ║
    \x1b[1;92m║➣WHATSAPP  : 01949*****              ║
    \x1b[1;92m║➣FACEBOOK  : MD SWAPON MIA           ║
    \x1b[1;92m║➣Group     : CYBER 71 SPAMMING SQUAD ║
    \x1b[1;92m║➣YOUTUBE   : [ TECHNOLOGY BOY ]      ║
-   \x1b[1;92m╚═══════════════════════════════════╝""")
+   \x1b[1;92m╚═════════════════════════════════════╝""")
 
 class Main:
 	def __init__(self):
